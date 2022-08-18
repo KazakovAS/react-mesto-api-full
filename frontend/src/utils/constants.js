333699578276297c04c0c2c2e8a052f63abbea8a
Kazakov Alexey <admin@kazakov-a.ru>
2022-08-19 01:22:54 +0300

    изменил домен

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-// const DOMAIN = 'api.lerush.students.nomoredomains.sbs';
-const DOMAIN = 'auth.nomoreparties.co';
+const DOMAIN = 'api.lerush.students.nomoredomains.sbs';
+// const DOMAIN = 'auth.nomoreparties.co';
 
 export default DOMAIN;
